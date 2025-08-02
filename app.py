7205ed692e506600d1d3a90aeac309a29b65b0d8
administrator <tzhang2024@gmail.com>
2025-08-03 01:31:42 +0800

    Step 1: Set up Flask app with hello_world and greet routes

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello World :)</h1>'
+
+
+@app.route('/greet')
+def greet():
+    return 'Hello'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
